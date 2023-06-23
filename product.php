6761668f0c64f2109616443dcccc431e53d20d3f
gilbertcoquilla <77886920+gilbertcoquilla@users.noreply.github.com>
2023-06-23 10:56:12 +0000

    quantity button to be fixed

--- product.php
+++ product.php
@@ -8,10 +8,15 @@ $supply_id = $_GET['productid'];
 $clinic_id = $_SESSION['clinic_id'];
 $userID = $_SESSION["id"];
 
+// To get the stocks left of the product
+$ret_c = mysqli_query($con, "SELECT Stocks FROM petsupplies WHERE SupplyID='$supply_id'");
+$cnt_c = 1;
+$row_c = mysqli_fetch_array($ret_c);
+
+// To check if the item is added to the cart
 $ret_a = mysqli_query($con, "SELECT * FROM orderdetails WHERE SupplyID='$supply_id' AND UserID='$userID'");
 $cnt_a = 1;
 $row_a = mysqli_num_rows($ret_a);
-
 
 if ($row_a > 0) {
     if (isset($_POST['submit'])) {
@@ -59,8 +64,7 @@ if ($row_a > 0) {
 <head>
     <meta charset="utf-8">
     <title>Paws N Pages | Clinic</title>
-    <link rel="icon" href="https://media.discordapp.net/attachments/1112075552669581332/1113455947420024832/icon.png"
-        type="image/x-icon">
+    <link rel="icon" href="https://media.discordapp.net/attachments/1112075552669581332/1113455947420024832/icon.png" type="image/x-icon">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
@@ -91,6 +95,7 @@ if ($row_a > 0) {
             value++;
             input.value = value;
         }
+
         function decreaseCount(a, b) {
             var input = b.nextElementSibling;
             var value = parseInt(input.value, 10);
@@ -306,7 +311,7 @@ if ($row_a > 0) {
         $row = mysqli_num_rows($ret);
         if ($row > 0) {
             while ($row = mysqli_fetch_array($ret)) {
-                ?>
+        ?>
                 <div class="product-container">
                     <?php if ($row['SupplyImage'] != "") {
                         echo '<img width="500" height="500" src="image_upload/' . $row['SupplyImage'] . '">';
@@ -336,15 +341,15 @@ if ($row_a > 0) {
                                 $row1 = mysqli_num_rows($ret1);
                                 if ($row1 > 0) {
                                     while ($row1 = mysqli_fetch_array($ret1)) {
-                                        ?>
+                                ?>
 
                                         <div class="counter">
                                             <span class="down" onClick='decreaseCount(event, this)'>-</span>
-                                            <input type="text" name="quantity" value="<?php echo $row1['Quantity'] ?>" min="1">
+                                            <input type="text" name="quantity" value="<?php echo $row1['Quantity'] ?>" min="1" max="<?php echo $row_c['Stocks']; ?>">
                                             <span class="up" onClick='increaseCount(event, this)'>+</span>
                                         </div>
 
-                                        <?php
+                                    <?php
                                         $cnt1 = $cnt1 + 1;
                                     }
                                 } else {
@@ -352,7 +357,7 @@ if ($row_a > 0) {
 
                                     <div class="counter">
                                         <span class="down" onClick='decreaseCount(event, this)'>-</span>
-                                        <input type="text" name="quantity" value="1" min="1">
+                                        <input type="text" name="quantity" value="1" min="1" max="<?php echo $row_c['Stocks']; ?>">
                                         <span class="up" onClick='increaseCount(event, this)'>+</span>
                                     </div>
 
@@ -379,7 +384,7 @@ if ($row_a > 0) {
                         </form>
                     </div>
                 </div>
-                <?php
+        <?php
                 $cnt = $cnt + 1;
             }
         }
@@ -392,8 +397,7 @@ if ($row_a > 0) {
 
 
     <!-- Modal Start -->
-    <div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="cartModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="cartModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -427,10 +431,8 @@ if ($row_a > 0) {
                 <div class="col-lg-4 col-md-6">
                     <h5 class="text-uppercase border-start border-5 border-primary ps-3 mb-4">Get In Touch</h5>
                     <p class="mb-4">If you have inquiries feel free to contact us below</p>
-                    <a class="mb-2" href="https://goo.gl/maps/nGdbiDamK7MP9L5z5"><i
-                            class="bi bi-geo-alt text-primary me-2"></i>Manila, PH</br></a>
-                    <a class="mb-2" href="mailto:pawsnpages.site@gmail.com"><i
-                            class="bi bi-envelope-open text-primary me-2"></i>pawsnpages.site@gmail.com</a>
+                    <a class="mb-2" href="https://goo.gl/maps/nGdbiDamK7MP9L5z5"><i class="bi bi-geo-alt text-primary me-2"></i>Manila, PH</br></a>
+                    <a class="mb-2" href="mailto:pawsnpages.site@gmail.com"><i class="bi bi-envelope-open text-primary me-2"></i>pawsnpages.site@gmail.com</a>
                     <a class="mb-0" href="tel:+6396176261"></br><i class="bi bi-telephone text-primary me-2"></i>+63 961
                         762 6162</a>
                 </div>
@@ -438,14 +440,10 @@ if ($row_a > 0) {
                     <h5 class="text-uppercase border-start border-5 border-primary ps-3 mb-4">Quick Links</h5>
                     <div class="d-flex flex-column justify-content-start">
                         <a class="text-body mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
-                        <a class="text-body mb-2" href="clinics.php"><i
-                                class="bi bi-arrow-right text-primary me-2"></i>Vet Clinics</a>
-                        <a class="text-body mb-2" href="index.php#services"><i
-                                class="bi bi-arrow-right text-primary me-2"></i>Our Services</a>
-                        <a class="text-body mb-2" href="index.php#founders"><i
-                                class="bi bi-arrow-right text-primary me-2"></i>Meet The Team</a>
-                        <a class="text-body" href="contact.php"><i
-                                class="bi bi-arrow-right text-primary me-2"></i>Contact Us</a>
+                        <a class="text-body mb-2" href="clinics.php"><i class="bi bi-arrow-right text-primary me-2"></i>Vet Clinics</a>
+                        <a class="text-body mb-2" href="index.php#services"><i class="bi bi-arrow-right text-primary me-2"></i>Our Services</a>
+                        <a class="text-body mb-2" href="index.php#founders"><i class="bi bi-arrow-right text-primary me-2"></i>Meet The Team</a>
+                        <a class="text-body" href="contact.php"><i class="bi bi-arrow-right text-primary me-2"></i>Contact Us</a>
                     </div>
                 </div>
 
@@ -484,7 +482,7 @@ if ($row_a > 0) {
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             var cartItems = [];
 
             function updateCart() {
@@ -508,7 +506,7 @@ if ($row_a > 0) {
                 $("#cartItems").html(cartContent);
             }
 
-            $(document).on("click", ".remove-btn", function () {
+            $(document).on("click", ".remove-btn", function() {
                 var index = $(this).data("index");
                 cartItems.splice(index, 1);
                 updateCart();
@@ -517,7 +515,7 @@ if ($row_a > 0) {
             // Rest of the JavaScript code remains the same
 
             function addToCart(name) {
-                var index = cartItems.findIndex(function (item) {
+                var index = cartItems.findIndex(function(item) {
                     return item.name === name;
                 });
                 if (index === -1) {
@@ -531,17 +529,17 @@ if ($row_a > 0) {
                 updateCart();
             }
 
-            $(document).on("click", "#openCartBtn", function () {
+            $(document).on("click", "#openCartBtn", function() {
                 updateCart();
                 $("#cartModal").modal("show");
             });
 
-            $(document).on("click", "#addToCart", function () {
+            $(document).on("click", "#addToCart", function() {
                 var productName = $(this).closest(".product-item").find("h6").text();
                 addToCart(productName);
             });
 
-            $(document).on("click", ".quantity-btn", function () {
+            $(document).on("click", ".quantity-btn", function() {
                 var index = $(this).data("index");
                 var action = $(this).data("action");
                 if (action === "increase") {
@@ -555,7 +553,7 @@ if ($row_a > 0) {
                 updateCart();
             });
 
-            $(document).on("click", "#checkoutBtn", function () {
+            $(document).on("click", "#checkoutBtn", function() {
                 // Handle checkout logic here
                 console.log("Checkout button clicked");
             });
@@ -564,14 +562,14 @@ if ($row_a > 0) {
     <!-- Add the JavaScript code at the bottom of your HTML file -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             // Close button click event
-            $('#cartModal .close').click(function () {
+            $('#cartModal .close').click(function() {
                 $('#cartModal').modal('hide');
             });
 
             // Checkout button click event
-            $('#checkoutBtn').click(function () {
+            $('#checkoutBtn').click(function() {
                 // Perform the checkout action here
                 // You can add your own code to handle the checkout process
                 alert('Checkout button clicked!');
@@ -580,7 +578,7 @@ if ($row_a > 0) {
     </script>
 
     <script>
-        document.getElementById('plus').onclick = function () {
+        document.getElementById('plus').onclick = function() {
             function add() {
                 var quantity = document.getElementById('quantity');
                 quantity.value += 1;
