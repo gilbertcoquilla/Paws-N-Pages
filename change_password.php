77f8db9d97da26877b262878dcc01e3198780835
gilbertcoquilla <77886920+gilbertcoquilla@users.noreply.github.com>
2023-07-13 16:23:22 +0000

    added validation for password

--- change_password.php
+++ change_password.php
@@ -4,9 +4,6 @@ include('connection.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<!--- NO BACKGROUND YET
- PHP NOT YET WORKING
- --->
 
 <head>
     <meta charset="utf-8">
@@ -25,7 +22,12 @@ include('connection.php');
 
     <!-- Icon Font Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css'>
     <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <!-- Newly added -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
     <!-- Libraries Stylesheet -->
     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
@@ -48,6 +50,38 @@ include('connection.php');
             height: 100vh;
             margin: 0;
             padding: 0;
+        }
+
+        .fa-eye {
+            position: absolute;
+            top: 28px;
+            right: 5%;
+            cursor: pointer;
+            color: lightgray;
+        }
+
+        /* For password validation (below) */
+
+        /* Add a green text color and a checkmark when the requirements are right */
+        .valid {
+            color: green;
+        }
+
+        .valid:before {
+            position: relative;
+            left: -3px;
+            content: "✔";
+        }
+
+        /* Add a red text color and an "x" when the requirements are wrong */
+        .invalid {
+            color: red;
+        }
+
+        .invalid:before {
+            position: relative;
+            left: -3px;
+            content: "✖";
         }
     </style>
 </head>
@@ -73,8 +107,15 @@ include('connection.php');
                                 <input type="text" name="username" id="username" class="form-control  bg-light border-3 px-4 py-3" style="border-radius:15px;" placeholder="Username" required>
                             </div>
                             <div class="col-12" style="padding-bottom: 10px;">
-                                <input type="password" name="newpass" id="newpass" class="form-control  bg-light border-3 px-4 py-3" style="border-radius:15px;" placeholder="New Password" required>
+                                <input type="password" name="newpass" id="newpass" minlength="8" maxlength="16" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" class="form-control bg-light border-3 px-4 py-3" style="border-radius:15px;" placeholder="New Password" required>
+                                <i class="fa-solid fa-eye" id="eye"></i>
                             </div>
+                            <p id="message" style="display: none;font-style: italic; font-size: 15px; padding-top: 10px;">
+                                &nbsp;&nbsp;&nbsp;&nbsp;<span id="length" class="invalid">*Minimum of (8)
+                                    characters</span>, <span id="capital" class="invalid">at least (1)
+                                    uppercase letter</span>, <span id="letter" class="invalid">(1) lowercase
+                                    letter</span> and <span id="number" class="invalid">(1) number</span>
+                            </p>
                             <div class="col-12">
                                 <button type="submit" name="submit" class="btn btn-primary w-100 py-3" style="border-radius:15px;">Submit</button>
                             </div>
@@ -160,6 +201,76 @@ include('connection.php');
 
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
+    <script>
+        // Show password
+        const passwordInput = document.querySelector("#newpass");
+        const eye = document.querySelector("#eye");
+
+        eye.addEventListener("click", function() {
+            this.classList.toggle("fa-eye-slash")
+            const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+            passwordInput.setAttribute("type", type)
+        });
+
+        // Password Validation
+        var myInput = document.getElementById("newpass");
+        var letter = document.getElementById("letter");
+        var capital = document.getElementById("capital");
+        var number = document.getElementById("number");
+        var length = document.getElementById("length");
+
+        // When the user clicks on the password field, show the message box
+        myInput.onfocus = function() {
+            document.getElementById("message").style.display = "block";
+        }
+
+        // When the user clicks outside of the password field, hide the message box
+        myInput.onblur = function() {
+            document.getElementById("message").style.display = "none";
+        }
+
+        // When the user starts to type something inside the password field
+        myInput.onkeyup = function() {
+            // Validate lowercase letters
+            var lowerCaseLetters = /[a-z]/g;
+            if (myInput.value.match(lowerCaseLetters)) {
+                letter.classList.remove("invalid");
+                letter.classList.add("valid");
+            } else {
+                letter.classList.remove("valid");
+                letter.classList.add("invalid");
+            }
+
+            // Validate capital letters
+            var upperCaseLetters = /[A-Z]/g;
+            if (myInput.value.match(upperCaseLetters)) {
+                capital.classList.remove("invalid");
+                capital.classList.add("valid");
+            } else {
+                capital.classList.remove("valid");
+                capital.classList.add("invalid");
+            }
+
+            // Validate numbers
+            var numbers = /[0-9]/g;
+            if (myInput.value.match(numbers)) {
+                number.classList.remove("invalid");
+                number.classList.add("valid");
+            } else {
+                number.classList.remove("valid");
+                number.classList.add("invalid");
+            }
+
+            // Validate length
+            if (myInput.value.length >= 8) {
+                length.classList.remove("invalid");
+                length.classList.add("valid");
+            } else {
+                length.classList.remove("valid");
+                length.classList.add("invalid");
+            }
+        }
+    </script>
 </body>
 
 </html>
